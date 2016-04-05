95adb4467e1b4f78a0f7ba28414090256332ab4c
Bulat Damdinov <dbulats88@gmail.com>
2016-04-05 18:50:27 +0800

    delete duplicate modules Gii and Debug for development enviroments

--- config/web.php
+++ config/web.php
@@ -82,16 +82,4 @@ $config = [
     },
 ];
 
-if (YII_ENV_DEV) {
-    $config['bootstrap'][] = 'debug';
-    $config['modules']['debug'] = [
-        'class' => 'yii\debug\Module',
-    ];
-
-    $config['bootstrap'][] = 'gii';
-    $config['modules']['gii'] = [
-        'class' => 'yii\gii\Module',
-    ];
-}
-
 return $config;
