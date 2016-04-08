01d7ede7d68814d022725f39c77fdc8c8f7ae2ca
Bulat Damdinov <dbulats88@gmail.com>
2016-04-08 22:06:08 +0800

    Delete commands/HelloController, create commands/InitController

---
+++ commands/InitController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace app\commands;
+
+use yii\console\Controller;
+
+class InitController extends Controller
+{
+    public function actionIndex()
+    {
+        echo "Hello, World!\n";
+    }
+}
