839ff03c0260c2359d2d272ffb907c71262b6c6f
Bulat Damdinov <dbulats88@gmail.com>
2016-04-09 14:30:30 +0800

    created BuryatName model

---
+++ models/BuryatName.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "buryat_name".
+ *
+ * @property integer $id
+ * @property string $name
+ * @property string $description
+ * @property string $note
+ * @property integer $male
+ * @property integer $female
+ */
+class BuryatName extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'buryat_name';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['male', 'female'], 'required'],
+            [['male', 'female'], 'integer'],
+            [['name', 'description'], 'string', 'max' => 255],
+            [['note'], 'string'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => Yii::t('app', 'ID'),
+            'name' => Yii::t('app', 'Name'),
+            'description' => Yii::t('app', 'Description'),
+            'note' => Yii::t('app', 'Note'),
+            'male' => Yii::t('app', 'Male'),
+            'female' => Yii::t('app', 'Female'),
+        ];
+    }
+}
