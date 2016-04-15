4b0f23b6904e25de7d701124e415d7c0f92a2bca
Bulat Damdinov <dbulats88@gmail.com>
2016-04-15 23:18:19 +0800

    change class btn for block buryat-name on the main page

--- views/site/index.php
+++ views/site/index.php
@@ -133,7 +133,7 @@ $this->title = Yii::$app->name . ' - ' . Yii::t('app', 'Russian-Buryat, Buryat-R
                                     <button type="button" class="btn btn-default add-bur-word">ү</button>
                                     <button type="button" class="btn btn-default add-bur-word">һ</button>
                                     <button type="button" class="btn btn-default add-bur-word">ө</button>
-                                    <button type="submit" class="btn btn-success"><?= Yii::t('app', 'Find') ?></button>
+                                    <button type="submit" class="btn btn-custom"><?= Yii::t('app', 'Find') ?></button>
                                 </span>
                                 </div>
                             </div>
