cfb3b54cc1a2804cb413e2055544e24ea61bb8bd
Bulat Damdinov <dbulats88@gmail.com>
2016-04-18 13:13:45 +0800

    change view for NewsWidget

--- views/news/_view.php
+++ views/news/_view.php
@@ -6,12 +6,14 @@ use yii\helpers\Html;
 ?>
 
 <div class="news-item">
-    <h3><?= Html::a(Html::encode($model->title), ['view', 'id' => $model->id]) ?></h3>
+    <h4><?= Html::a(Html::encode($model->title), ['view', 'id' => $model->id]) ?></h4>
     <p class="text-danger"><?= Yii::$app->formatter->asDate($model->created_at) ?></p>
     <p class="description">
         <?= nl2br(Html::encode($model->description)) ?>
     </p>
-    <?= Html::a(Yii::t('app', 'Read more'), ['/news/view', 'id' => $model->id], ['class' => 'btn btn-default btn-sm']) ?>
+    <?= Html::a(Yii::t('app', 'Read more') . ' →',
+        ['/news/view', 'id' => $model->id],
+        ['class' => 'btn btn-default btn-sm']) ?>
 </div>
 
 
--- widgets/views/news.php
+++ widgets/views/news.php
@@ -1,19 +1,11 @@
 <?php
 
-use yii\helpers\Html;
-
 /* @var $model \app\models\News */
+
 ?>
 
 <h3><?= Yii::t('app', 'News') ?></h3>
 
 <?php foreach ($model as $news): ?>
-    <div class="news-item">
-        <h4><?= Html::a(Html::encode($news->title), ['news/view', 'id' => $news->id]) ?></h4>
-        <p class="text-danger"><?= Yii::$app->formatter->asDate($news->created_at) ?></p>
-        <p class="description">
-            <?= nl2br(Html::encode($news->description)) ?>
-        </p>
-        <?= Html::a(Yii::t('app', 'Read more'), ['/news/view', 'id' => $news->id], ['class' => 'btn btn-default btn-sm']) ?>
-    </div>
+    <?= $this->render('/news/_view', ['model' => $news]) ?>
 <?php endforeach ?>
