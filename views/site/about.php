c6cc4a2d63c8baca84422425d917b60c6cf5ab51
Bulat Damdinov <dbulats88@gmail.com>
2016-04-19 15:50:34 +0800

    added comment block in About page

--- views/site/about.php
+++ views/site/about.php
@@ -9,4 +9,18 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-about">
     <h1><?= Html::encode($this->title) ?></h1>
+
+    <div class="content-text">
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi consequuntur deleniti deserunt et eveniet expedita fuga, ipsum laborum laudantium molestiae nihil nostrum odit omnis placeat quae quas repellat repellendus vel!
+        </p>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-8 col-sm-offset-2">
+            <div class="mt20 comment-block">
+                <?= $this->render('//_comments') ?>
+            </div>
+        </div>
+    </div>
 </div>
