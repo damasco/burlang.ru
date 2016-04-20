18ee8754a87c2a0abc5e6180ab320757d99e45b8
Bulat Damdinov <dbulats88@gmail.com>
2016-04-20 12:09:34 +0800

    changed title for news/update page

--- views/news/update.php
+++ views/news/update.php
@@ -5,9 +5,7 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model app\models\News */
 
-$this->title = Yii::t('app', 'Update {modelClass}: ', [
-    'modelClass' => Yii::t('app', 'News'),
-]) . $model->title;
+$this->title = Yii::t('app', 'Update') . ': ' . $model->title;
 $this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'News'), 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => $model->title, 'url' => ['view', 'id' => $model->id]];
 $this->params['breadcrumbs'][] = Yii::t('app', 'Update');
