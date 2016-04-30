c0cf1a0176c1f6bbb23e10a92c54bb24e8da9b75
Bulat Damdinov <dbulats88@gmail.com>
2016-04-30 16:09:30 +0800

    added buttons with buryat words for burwords form

--- views/burwords/_form.php
+++ views/burwords/_form.php
@@ -12,7 +12,18 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'name', ['template' => '
+        {label}
+        <div class="input-group">
+            {input}
+            <span class="input-group-btn">
+                <button type="button" class="btn btn-default add-bur-word">ү</button>
+                <button type="button" class="btn btn-default add-bur-word">һ</button>
+                <button type="button" class="btn btn-default add-bur-word">ө</button>
+            </span>
+        </div>
+        {error}{hint}
+    '])->textInput(['maxlength' => true]) ?>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? Yii::t('app', 'Create') : Yii::t('app', 'Save'),
