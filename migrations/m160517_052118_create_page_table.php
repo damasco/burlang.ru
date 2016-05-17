39038ac32eda037f265693123543e0a315409abb
Bulat Damdinov <dbulats88@gmail.com>
2016-05-17 13:24:00 +0800

    create page table

---
+++ migrations/m160517_052118_create_page_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation for table `page_table`.
+ */
+class m160517_052118_create_page_table extends Migration
+{
+    protected $tableName = '{{%page}}';
+    /**
+     * @inheritdoc
+     */
+    public function up()
+    {
+        $this->createTable($this->tableName, [
+            'id' => $this->primaryKey(),
+            'title' => $this->string()->notNull(),
+            'description' => $this->string()->notNull(),
+            'content' => $this->text()->notNull(),
+            'created_at' => $this->integer()->notNull(),
+            'updated_at' => $this->integer()->notNull()
+        ]);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function down()
+    {
+        $this->dropTable($this->tableName);
+    }
+}
