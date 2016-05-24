9f3b72b0b9317d46fd540d5669f9847de37c86fe
Bulat Damdinov <dbulats88@gmail.com>
2016-05-24 17:36:07 +0800

    changed test files HomeCept for acceptance and functional

--- tests/codeception/functional/HomeCept.php
+++ tests/codeception/functional/HomeCept.php
@@ -5,7 +5,7 @@
 $I = new FunctionalTester($scenario);
 $I->wantTo('ensure that home page works');
 $I->amOnPage(Yii::$app->homeUrl);
-$I->see('My Company');
-$I->seeLink('About');
-$I->click('About');
-$I->see('This is the About page.');
+$I->see('Burlang');
+$I->seeLink('Login');
+$I->click('Login');
+$I->see('Sign in');
