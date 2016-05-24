9699336534dffe58da8961f2d3c0c74a4d3b9a45
Bulat Damdinov <dbulats88@gmail.com>
2016-05-24 18:06:55 +0800

    changed acceptance/HomeCept test

--- tests/codeception/acceptance/HomeCept.php
+++ tests/codeception/acceptance/HomeCept.php
@@ -4,8 +4,14 @@
 
 $I = new AcceptanceTester($scenario);
 $I->wantTo('ensure that home page works');
+
 $I->amOnPage(Yii::$app->homeUrl);
 $I->see('Burlang');
 $I->seeLink('Login');
 $I->click('Login');
+
+if (method_exists($I, 'wait')) {
+    $I->wait(1); // only for selenuim
+}
+
 $I->see('Sign in');
