f524c82d6f325c4cd862a0502fb9b47f2ecada33
Bulat Damdinov <dbulats88@gmail.com>
2016-06-02 21:09:18 +0800

    added BuraytWordTest unit test file

---
+++ tests/codeception/unit/models/BuraytWordTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace test\codeception\unit;
+
+use app\models\BuryatWord;
+use Codeception\Specify;
+use yii\codeception\DbTestCase;
+
+class BuraytWordTest extends DbTestCase
+{
+    use Specify;
+
+    public function testRules()
+    {
+        $model = new BuryatWord([
+            'name' => 'Given name',
+        ]);
+
+        expect('model is valid', $model->validate())->true();
+
+        $model->name = '';
+
+        expect('model is not valid', $model->validate())->false();
+        expect('name is required', $model->errors)->hasKey('name');
+    }
+
+}
