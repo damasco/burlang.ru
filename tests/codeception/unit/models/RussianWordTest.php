77efacddb54abd9e66d65b7b53b9231129a7f156
Bulat Damdinov <dbulats88@gmail.com>
2016-06-05 00:23:42 +0800

    added RussianWordTest unit test file

---
+++ tests/codeception/unit/models/RussianWordTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace test\codeception\unit;
+
+use app\models\RussianWord;
+use Codeception\Specify;
+use yii\codeception\DbTestCase;
+
+class RussianWordTest extends DbTestCase
+{
+    use Specify;
+
+    public function testRules()
+    {
+        $model = new RussianWord([
+            'name' => 'Given name',
+        ]);
+
+        expect('model is valid', $model->validate())->true();
+
+        $model->name = '';
+
+        expect('model is not valid', $model->validate())->false();
+        expect('name is required', $model->errors)->hasKey('name');
+    }
+
+}
