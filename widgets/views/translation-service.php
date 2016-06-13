09e1f35414dc3da439b4b5aa642c88989653a5ad
Bulat Damdinov <dbulats88@gmail.com>
2016-06-13 13:58:08 +0800

    changed link on the view TranslationWidget

--- widgets/views/translation-service.php
+++ widgets/views/translation-service.php
@@ -4,6 +4,7 @@ use yii\helpers\Html;
 
 /* @var \app\models\Page $model */
 ?>
+
 <div class="translation-service-widget bg-warning text-center">
 
     <h4><?= $model->title ?></h4>
@@ -12,6 +13,6 @@ use yii\helpers\Html;
         <?= $model->description ?>
     </p>
 
-    <?= Html::a(Yii::t('app', 'More'), ['/page/' . $model->link], ['class' => 'btn btn-success btn-sm']) ?>
+    <?= Html::a(Yii::t('app', 'More'), ['/page/view', 'link' => $model->link], ['class' => 'btn btn-success btn-sm']) ?>
 
 </div>
