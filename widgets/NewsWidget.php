697ce88a5102c41b4e855b950c2e9da0902849ea
Bulat Damdinov <dbulats88@gmail.com>
2016-06-15 13:06:08 +0800

    added condition active for NewsWidget

--- widgets/NewsWidget.php
+++ widgets/NewsWidget.php
@@ -2,6 +2,7 @@
 
 namespace app\widgets;
 
+use Yii;
 use app\models\News;
 use yii\base\Widget;
 
@@ -9,7 +10,11 @@ class NewsWidget extends Widget
 {
     public function run()
     {
-        $model = News::find()->where(['active' => 1])->orderBy('created_at DESC')->limit(3)->all();
+        $query = News::find();
+        if (Yii::$app->user->isGuest) {
+            $query->where(['active' => 1]);
+        }
+        $model = $query->orderBy('created_at DESC')->limit(3)->all();
 
         return $this->render('news', [
             'model' => $model
