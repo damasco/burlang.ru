f720b6b55d74276e3cd10a082c1d9cb730db3c97
Bulat Damdinov <dbulats88@gmail.com>
2016-07-01 00:51:39 +0800

    add column slug for book_chapter table

---
+++ migrations/m160630_164523_add_column_slug_book_chapter.php
@@ -0,0 +1,24 @@
+<?php
+
+use yii\db\Migration;
+
+class m160630_164523_add_column_slug_book_chapter extends Migration
+{
+    protected $tableName = '{{%book_chapter}}';
+
+    /**
+     * @inheritdoc
+     */
+    public function up()
+    {
+        $this->addColumn($this->tableName, 'slug', $this->string()->notNull()->after('title'));
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function down()
+    {
+        $this->dropColumn($this->tableName, 'slug');
+    }
+}
