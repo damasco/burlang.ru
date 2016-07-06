79d933aabca7ae7def616e71d12c555c453f5783
Bulat Damdinov <dbulats88@gmail.com>
2016-07-06 23:38:10 +0800

    added test file acceptance BookCest

---
+++ tests/codeception/acceptance/BookCest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace test\codeception\acceptance;
+
+use AcceptanceTester;
+use Yii;
+
+class BookCest
+{
+    public function indexPage(AcceptanceTester $I)
+    {
+        $I->wantTo('ensure that books page works');
+
+        $I->amOnPage(Yii::$app->homeUrl);
+        $I->seeLink('Books');
+        $I->click('Books');
+
+        if (method_exists($I, 'wait')) {
+            $I->wait(1); // only for selenuim
+        }
+        $I->see('Books', 'h1');
+    }
+}
