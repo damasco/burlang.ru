c3917c130faf261a68752baf121405e34cb3251d
Bulat Damdinov <dbulats88@gmail.com>
2016-07-06 08:56:38 +0800

    added unit test file BookTest

---
+++ tests/codeception/unit/models/BookTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace test\codeception\unit;
+
+use app\models\Book;
+use Codeception\Specify;
+use yii\codeception\DbTestCase;
+
+class BookTest extends DbTestCase
+{
+    use Specify;
+
+    public function testRules()
+    {
+        $model = new Book([
+            'title' => 'Title',
+            'description' => 'Description',
+            'content' => 'Content',
+            'active' => 1,
+        ]);
+
+        expect('model is valid', $model->validate())->true();
+
+        $model->title = '';
+        $model->description = '';
+        $model->content = '';
+        $model->active = '';
+
+        expect('model is not valid', $model->validate())->false();
+        expect('title is required', $model->errors)->hasKey('title');
+        expect('description is not required', $model->errors)->hasntKey('description');
+        expect('content is not required', $model->errors)->hasntKey('content');
+        expect('active is required', $model->errors)->hasKey('active');
+
+    }
+
+}
