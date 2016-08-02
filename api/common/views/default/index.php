0be21a0687b79c3d8c867127a275b7f1e6059ed8
Bulat Damdinov <dbulats88@gmail.com>
2016-08-02 20:31:53 +0800

    changed view files for api and v1 modules

--- api/common/views/default/index.php
+++ api/common/views/default/index.php
@@ -1,3 +1,19 @@
+<?php 
+
+use yii\helpers\Html;
+
+/**
+ * @var yii\web\View $this
+ */
+
+$this->title = 'Api';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+
 <div class="api-default-index">
-    <h1>API</h1>
+    <h1><?= $this->title ?></h1>
+
+	<h3>
+		<?= Html::a('v1', ['/api/v1']) ?>
+	</h3>
 </div>
--- api/v1/views/default/index.php
+++ api/v1/views/default/index.php
@@ -1,4 +1,15 @@
+<?php
+
+/**
+ * @var yii\web\View $this
+ */
+
+$this->title = 'v1';
+$this->params['breadcrumbs'][] = ['label' => 'Api', 'url' => ['/api/default/index']];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+
 <div class="v1-default-index">
-    <h1>V1</h1>
+    <h1><?= $this->title ?></h1>
 
 </div>
