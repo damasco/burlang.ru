df08594181bfa918bbedd658d52239c61f245600
Bulat Damdinov <dbulats88@gmail.com>
2016-08-03 11:13:38 +0800

    added example request for api v1

--- api/v1/views/default/index.php
+++ api/v1/views/default/index.php
@@ -12,4 +12,13 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="v1-default-index">
     <h1><?= $this->title ?></h1>
 
+    <h4>Example:</h4>
+    <ul>
+    	<li>
+    		<code>/api/v1/buryat-word?word=сайн</code>
+    	</li>
+    	<li>
+    		<code>/api/v1/russian-word?word=привет</code>
+    	</li>
+    </ul>
 </div>
