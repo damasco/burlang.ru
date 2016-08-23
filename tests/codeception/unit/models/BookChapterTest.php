668b0515754d89c5644b871a16e2c764466a454d
Bulat Damdinov <dbulats88@gmail.com>
2016-08-23 17:13:40 +0800

    added unit test file BookChapterTest

---
+++ tests/codeception/unit/models/BookChapterTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace test\codeception\unit;
+
+use Yii;
+use app\models\Book;
+use app\models\BookChapter;
+use Codeception\Specify;
+use yii\codeception\DbTestCase;
+
+class BookChapterTest extends DbTestCase
+{
+    use Specify;
+
+    public function testRules()
+    {
+        Book::deleteAll(['title' => 'Title']);
+
+        $book = new Book([
+            'title' => 'Title',
+            'description' => 'Description',
+            'content' => 'Content',
+            'active' => 1,
+        ]);
+
+        $book->save();
+
+        $bookChapter = new BookChapter([
+            'title' => 'Title',
+            'slug' => 'title',
+            'content' => 'Content',
+            'book_id' => $book->id,
+        ]);
+
+        expect('model is valid', $bookChapter->validate())->true();
+        
+        $bookChapter->title = '';
+        $bookChapter->content = '';
+        $bookChapter->book_id = '';
+
+        expect('model is not valid', $bookChapter->validate())->false();
+        expect('title is required', $bookChapter->errors)->hasKey('title');
+        expect('content is not required', $bookChapter->errors)->hasntKey('content');
+        expect('book_id is required', $bookChapter->errors)->hasKey('book_id');
+
+        $book->delete();
+    }
+}
