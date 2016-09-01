5947e3923d1b043980c54c17b76993afaa63669d
Bulat Damdinov <dbulats88@gmail.com>
2016-09-01 22:23:53 +0800

    changed api doc index page

---
+++ api/v1/views/default/_view.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\helpers\Url;
+use yii\widgets\Menu;
+
+/**
+ * @var string $title
+ * @var array $links
+ */
+
+$item = [];
+foreach ($links as $link) {
+    $items[] = ['label' => Url::to($link, true)];
+}
+?>
+
+<div class="panel panel-default">
+    <div class="panel-heading">
+        <h4 class="panel-title">
+            <?= Yii::t('app', 'Names') ?>
+        </h4>
+    </div>
+    <div class="panel-body">
+        <?= Menu::widget([
+            'items' => $items,
+            'labelTemplate' => '<code>{label}</code>',
+        ]) ?>
+    </div>
+</div>
--- api/v1/views/default/index.php
+++ api/v1/views/default/index.php
@@ -1,7 +1,5 @@
 <?php
 
-use yii\helpers\Url;
-
 /**
  * @var yii\web\View $this
  */
@@ -13,57 +11,27 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="v1-default-index">
     <h1><?= $this->title ?></h1>
 
-    <div class="panel panel-default">
-        <div class="panel-heading">
-            <h4 class="panel-title">
-                <?= Yii::t('app', 'Names') ?>
-            </h4>
-        </div>
-        <div class="panel-body">
-            <ul>
-                <li>
-                    <code><?= Url::to(['/v1/buryat-name/get-names', 'q' => 'begin_search_name'], true) ?></code>
-                </li>
-                <li>
-                    <code><?= Url::to(['/v1/buryat-name/view', 'name' => 'Name'], true) ?></code>
-                </li>
-            </ul>
-        </div>
-    </div>
+    <?= $this->render('_view', [
+        'title' => Yii::t('app', 'Names'),
+        'links' => [
+            ['/v1/buryat-name/get-names', 'q' => 'begin_search_name'],
+            ['/v1/buryat-name/view', 'name' => 'Name'],
+        ]
+    ]) ?>
 
-    <div class="panel panel-default">
-        <div class="panel-heading">
-            <h4 class="panel-title">
-                <?= Yii::t('app', 'Buryat words') ?>
-            </h4>
-        </div>
-        <div class="panel-body">
-            <ul>
-                <li>
-                    <code><?= Url::to(['/v1/buryat-word/get-words', 'q' => 'begin_search_word'], true) ?></code>
-                </li>
-                <li>
-                    <code><?= Url::to(['/v1/buryat-word/translate', 'word' => 'Word'], true) ?></code>
-                </li>
-            </ul>
-        </div>
-    </div>
+    <?= $this->render('_view', [
+        'title' => Yii::t('app', 'Buryat words'),
+        'links' => [
+            ['/v1/buryat-word/get-words', 'q' => 'begin_search_word'],
+            ['/v1/buryat-word/translate', 'word' => 'Word'],
+        ]
+    ]) ?>
 
-    <div class="panel panel-default">
-        <div class="panel-heading">
-            <h4 class="panel-title">
-                <?= Yii::t('app', 'Russian words') ?>
-            </h4>
-        </div>
-        <div class="panel-body">
-            <ul>
-                <li>
-                    <code><?= Url::to(['/v1/russian-word/get-words', 'q' => 'begin_search_word'], true) ?></code>
-                </li>
-                <li>
-                    <code><?= Url::to(['/v1/russian-word/translate', 'word' => 'Word'], true) ?></code>
-                </li>
-            </ul>  
-        </div>
-    </div>
+    <?= $this->render('_view', [
+        'title' => Yii::t('app', 'Russian words'),
+        'links' => [
+            ['/v1/russian-word/get-words', 'q' => 'begin_search_word'],
+            ['/v1/russian-word/translate', 'word' => 'Word'],
+        ]
+    ]) ?>
 </div>
