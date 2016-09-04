a01fb957518bd26e7f1c86543d67bca9fc365b3f
Bulat Damdinov <dbulats88@gmail.com>
2016-09-04 14:28:37 +0800

    changed search form for buryat_name

--- views/buryat-name/_search.php
+++ views/buryat-name/_search.php
@@ -11,37 +11,40 @@ use app\widgets\ChartsInputWidget;
  */
 ?>
 
-<div class="buryat-word-search well">
-
-    <?php $form = ActiveForm::begin([
-        'action' => ['admin'],
-        'method' => 'get',
-    ]); ?>
-
-    <div class="row">
-        <div class="col-sm-6 col-md-4">
-            <?= $form->field($model, 'name')->widget(ChartsInputWidget::className()) ?>
+<div class="buryat-word-search">
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <h4 class="panel-title"><?= Yii::t('app', 'Search') ?></h4>
         </div>
-        <div class="col-sm-6 col-md-4">
-            <?= $form->field($model, 'description') ?>
-        </div>
-        <div class="col-md-4">
+        <div class="panel-body">
+            <?php $form = ActiveForm::begin([
+                'action' => ['admin'],
+                'method' => 'get',
+            ]); ?>
+
             <div class="row">
-                <div class="col-sm-6 col-xs-6">
-                    <?= $form->field($model, 'male')->dropDownList(['0' => Yii::t('app', 'No'), '1' => Yii::t('app', 'Yes')], ['prompt' => '']) ?>
+                <div class="col-sm-6 col-md-4">
+                    <?= $form->field($model, 'name')->widget(ChartsInputWidget::className()) ?>
                 </div>
-                <div class="col-sm-6 col-xs-6">
-                    <?= $form->field($model, 'female')->dropDownList(['0' => Yii::t('app', 'No'), '1' => Yii::t('app', 'Yes')], ['prompt' => '']) ?>
+                <div class="col-sm-6 col-md-4">
+                    <?= $form->field($model, 'description') ?>
+                </div>
+                <div class="col-md-4">
+                    <div class="row">
+                        <div class="col-sm-6 col-xs-6">
+                            <?= $form->field($model, 'male')->dropDownList(['0' => Yii::t('app', 'No'), '1' => Yii::t('app', 'Yes')], ['prompt' => '']) ?>
+                        </div>
+                        <div class="col-sm-6 col-xs-6">
+                            <?= $form->field($model, 'female')->dropDownList(['0' => Yii::t('app', 'No'), '1' => Yii::t('app', 'Yes')], ['prompt' => '']) ?>
+                        </div>
+                    </div>
                 </div>
             </div>
+
+            <?= Html::submitButton(Yii::t('app', 'Find'), ['class' => 'btn btn-primary']) ?>
+            <?= Html::resetButton(Yii::t('app', 'Reset'), ['class' => 'btn btn-default']) ?>
+
+            <?php ActiveForm::end(); ?>
         </div>
     </div>
-
-    <div class="form-group">
-        <?= Html::submitButton(Yii::t('app', 'Search'), ['class' => 'btn btn-primary']) ?>
-        <?= Html::resetButton(Yii::t('app', 'Reset'), ['class' => 'btn btn-default']) ?>
-    </div>
-
-    <?php ActiveForm::end(); ?>
-
 </div>
