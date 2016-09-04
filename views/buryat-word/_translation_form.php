2a29de69f57ee1a1e3a8bea619ff7be2b78e8a87
Bulat Damdinov <dbulats88@gmail.com>
2016-09-04 14:18:16 +0800

    changed _translation_form for buryat_word and russian_word

--- views/buryat-word/_translation_form.php
+++ views/buryat-word/_translation_form.php
@@ -14,74 +14,72 @@ use app\widgets\ChartsInputWidget;
  */
 ?>
 
+<h4><?= Yii::t('app', 'Translations') ?></h4>
+
+<div class="table-responsive">
+    <?= GridView::widget([
+        'dataProvider' => (new ActiveDataProvider([
+            'query' => $model->getTranslations(),
+            'pagination' => false
+        ])),
+        'summary' => false,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+            'name',
+            [
+                'attribute' => 'dict_id',
+                'value' => 'dictionary.name',
+                'visible' => !Yii::$app->devicedetect->isMobile() ? true : false,
+            ],
+            [
+                'class' => 'yii\grid\ActionColumn',
+                'template' => '{delete}',
+                'buttons' => [
+                    'delete' => function ($url, $model) {
+                        return Html::a(
+                            Html::icon('trash'),
+                            ['delete-translation', 'id' => $model->id],
+                            [
+                                'title' => Yii::t('app', 'Delete translation'),
+                                'class' => 'btn btn-sm btn-danger',
+                                'data' => [
+                                    'confirm' => Yii::t('app', 'Are you sure you want to delete this item?'),
+                                    'method' => 'post',
+                                ]
+                            ]
+                        );
+                    }
+                ],
+                'contentOptions' => [
+                    'class' => 'action-column-1',
+                ]
+            ],
+        ],
+    ]); ?>
+</div>
+
 <div class="panel panel-default">
     <div class="panel-heading">
-        <h4 class="panel-title"><?= Yii::t('app', 'Translations') ?></h4>
+        <h4 class="panel-title"><?= Yii::t('app', 'Add translation') ?></h4>
     </div>
     <div class="panel-body">
-        <div class="table-responsive">
-            <?= GridView::widget([
-                'dataProvider' => (new ActiveDataProvider([
-                    'query' => $model->getTranslations(),
-                    'pagination' => false
-                ])),
-                'summary' => false,
-                'columns' => [
-                    ['class' => 'yii\grid\SerialColumn'],
-                    'name',
-                    [
-                        'attribute' => 'dict_id',
-                        'value' => 'dictionary.name',
-                        'visible' => !Yii::$app->devicedetect->isMobile() ? true : false,
-                    ],
-                    [
-                        'class' => 'yii\grid\ActionColumn',
-                        'template' => '{delete}',
-                        'buttons' => [
-                            'delete' => function ($url, $model) {
-                                return Html::a(
-                                    Html::icon('trash'),
-                                    ['delete-translation', 'id' => $model->id],
-                                    [
-                                        'title' => Yii::t('app', 'Delete translation'),
-                                        'class' => 'btn btn-sm btn-danger',
-                                        'data' => [
-                                            'confirm' => Yii::t('app', 'Are you sure you want to delete this item?'),
-                                            'method' => 'post',
-                                        ]
-                                    ]
-                                );
-                            }
-                        ],
-                        'contentOptions' => [
-                            'class' => 'action-column-1',
-                        ]
-                    ],
-                ],
-            ]); ?>
-        </div>
+        <?php $form = ActiveForm::begin() ?>
 
-        <div class="well">
-            <h4><?= Yii::t('app', 'Add translation') ?></h4>
-            <hr>
-            <?php $form = ActiveForm::begin() ?>
-
-            <div class="row">
-                <div class="col-sm-6">
-                    <?= $form->field($translationForm, 'name')->widget(ChartsInputWidget::className()) ?>
-                </div>
-                <div class="col-sm-6">
-                    <?= $form->field($translationForm, 'dict_id')->dropDownList(
-                        ArrayHelper::map($dictionaries, 'id', 'name'), ['prompt' => '-']
-                    ) ?>
-                </div>
+        <div class="row">
+            <div class="col-sm-6">
+                <?= $form->field($translationForm, 'name')->widget(ChartsInputWidget::className()) ?>
             </div>
-
-            <?= $form->field($translationForm, 'burword_id')->hiddenInput(['value' => $model->id])->label(false) ?>
-
-            <?= Html::submitButton(Html::icon('plus') . ' ' . Yii::t('app', 'Add'), ['class' => 'btn btn-success']) ?>
-
-            <?php ActiveForm::end() ?>
+            <div class="col-sm-6">
+                <?= $form->field($translationForm, 'dict_id')->dropDownList(
+                    ArrayHelper::map($dictionaries, 'id', 'name'), ['prompt' => '-']
+                ) ?>
+            </div>
         </div>
+
+        <?= $form->field($translationForm, 'burword_id')->hiddenInput(['value' => $model->id])->label(false) ?>
+
+        <?= Html::submitButton(Html::icon('plus') . ' ' . Yii::t('app', 'Add'), ['class' => 'btn btn-success']) ?>
+
+        <?php ActiveForm::end() ?>
     </div>
 </div>
--- views/russian-word/_translation_form.php
+++ views/russian-word/_translation_form.php
@@ -12,62 +12,60 @@ use app\widgets\ChartsInputWidget;
  */
 ?>
 
+<h4><?= Yii::t('app', 'Translations') ?></h4>
+
+<div class="table-responsive">
+    <?= GridView::widget([
+        'dataProvider' => (new ActiveDataProvider([
+            'query' => $model->getTranslations(),
+            'pagination' => false
+        ])),
+        'summary' => false,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+            'name',
+            [
+                'class' => 'yii\grid\ActionColumn',
+                'template' => '{delete}',
+                'buttons' => [
+                    'delete' => function ($url, $model) {
+                        return Html::a(
+                            Html::icon('trash'),
+                            ['delete-translation', 'id' => $model->id],
+                            [
+                                'title' => Yii::t('app', 'Delete translation'),
+                                'class' => 'btn btn-sm btn-danger',
+                                'data' => [
+                                    'confirm' => Yii::t('app', 'Are you sure you want to delete this item?'),
+                                    'method' => 'post',
+                                ]
+                            ]
+                        );
+                    }
+                ],
+                'contentOptions' => [
+                    'class' => 'action-column-1'
+                ]
+            ],
+        ],
+    ]); ?>
+</div>
+
 <div class="panel panel-default">
     <div class="panel-heading">
-        <h4 class="panel-title"><?= Yii::t('app', 'Translations') ?></h4>
+        <h4 class="panel-title"><?= Yii::t('app', 'Add translation') ?></h4>
     </div>
     <div class="panel-body">
-        <div class="table-responsive">
-            <?= GridView::widget([
-                'dataProvider' => (new ActiveDataProvider([
-                    'query' => $model->getTranslations(),
-                    'pagination' => false
-                ])),
-                'summary' => false,
-                'columns' => [
-                    ['class' => 'yii\grid\SerialColumn'],
-                    'name',
-                    [
-                        'class' => 'yii\grid\ActionColumn',
-                        'template' => '{delete}',
-                        'buttons' => [
-                            'delete' => function ($url, $model) {
-                                return Html::a(
-                                    Html::icon('trash'),
-                                    ['delete-translation', 'id' => $model->id],
-                                    [
-                                        'title' => Yii::t('app', 'Delete translation'),
-                                        'class' => 'btn btn-sm btn-danger',
-                                        'data' => [
-                                            'confirm' => Yii::t('app', 'Are you sure you want to delete this item?'),
-                                            'method' => 'post',
-                                        ]
-                                    ]
-                                );
-                            }
-                        ],
-                        'contentOptions' => [
-                            'class' => 'action-column-1'
-                        ]
-                    ],
-                ],
-            ]); ?>
-        </div>
+        <?php $form = ActiveForm::begin() ?>
 
-        <div class="well">
-            <h4><?= Yii::t('app', 'Add translation') ?></h4>
-            <hr>
-            <?php $form = ActiveForm::begin() ?>
+        <?= $form->field($translationForm, 'name')->widget(
+            ChartsInputWidget::className(), ['options' => ['maxlength' => true]]
+        ) ?>
 
-            <?= $form->field($translationForm, 'name')->widget(
-                ChartsInputWidget::className(), ['options' => ['maxlength' => true]]
-            ) ?>
+        <?= $form->field($translationForm, 'ruword_id')->hiddenInput(['value' => $model->id])->label(false) ?>
 
-            <?= $form->field($translationForm, 'ruword_id')->hiddenInput(['value' => $model->id])->label(false) ?>
+        <?= Html::submitButton(Html::icon('plus') . ' ' . Yii::t('app', 'Add'), ['class' => 'btn btn-success']) ?>
 
-            <?= Html::submitButton(Html::icon('plus') . ' ' . Yii::t('app', 'Add'), ['class' => 'btn btn-success']) ?>
-
-            <?php ActiveForm::end() ?>
-        </div>
+        <?php ActiveForm::end() ?>
     </div>
 </div>
