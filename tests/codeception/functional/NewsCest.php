bc642e5ed77743322880513609797f1f42ef054e
Bulat Damdinov <dbulats88@gmail.com>
2016-09-18 00:29:46 +0800

    changed functional NewsCest test file

--- tests/codeception/functional/NewsCest.php
+++ tests/codeception/functional/NewsCest.php
@@ -4,8 +4,9 @@ namespace tests\codeception\functional;
 
 use FunctionalTester;
 use Yii;
+use yii\helpers\Url;
 
-class NewsCest
+class NewsCest extends FunctionalCest
 {
     public function indexPage(FunctionalTester $I)
     {
@@ -16,5 +17,41 @@ class NewsCest
         $I->click('News');
 
         $I->see('News', 'h1');
+    }
+    
+    /**
+     * @before loginAsAdmin
+     * @after logout
+     */
+    public function createPageAsAdmin(FunctionalTester $I)
+    {
+        $I->wantTo('ensure that news create page works for admin');
+        
+        $I->amOnPage(Url::to(['/news/create']));
+        $I->seeInTitle('Create news');
+    }
+    
+    /**
+     * @before loginAsModerator
+     * @after logout
+     */
+    public function createPageAsModerator(FunctionalTester $I)
+    {
+        $I->wantTo('ensure that news create page not works for moderator');
+        
+        $I->amOnPage(Url::to(['/news/create']));
+        $I->seeInTitle('Forbidden');
+    }
+    
+    /**
+     * @before loginAsUser
+     * @after logout
+     */
+    public function createPageAsUser(FunctionalTester $I)
+    {
+        $I->wantTo('ensure that news create page not works for simple user');
+        
+        $I->amOnPage(Url::to(['/news/create']));
+        $I->seeInTitle('Forbidden');
     }
 }
