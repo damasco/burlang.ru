51e0253a755732be832117fc85b7acdbfa8c2e47
Bulat Damdinov <dbulats88@gmail.com>
2016-09-17 17:17:15 +0800

    changed unit PageTest

--- tests/codeception/unit/models/PageTest.php
+++ tests/codeception/unit/models/PageTest.php
@@ -9,6 +9,15 @@ use yii\codeception\DbTestCase;
 class PageTest extends DbTestCase
 {
     use Specify;
+    
+    protected function setUp() {
+        parent::setUp();
+        Page::deleteAll([
+            'or', 
+            ['link' => 'unique-page-name'],
+            ['link' => 'page-name']
+        ]);
+    }
 
     public function testRules()
     {
@@ -18,7 +27,8 @@ class PageTest extends DbTestCase
             'link' => 'page-name',
             'description' => 'Description',
             'content' => 'Content',
-            'active' => 1
+            'active' => 1,
+            'static' => 1
         ]);
 
         expect('model is valid', $model->validate())->true();
@@ -28,12 +38,76 @@ class PageTest extends DbTestCase
         $model->link = '';
         $model->description = '';
         $model->content = '';
+        $model->active = '';
+        $model->static = '';
 
         expect('model is not valid', $model->validate())->false();
         expect('menu_name is required', $model->errors)->hasKey('menu_name');
         expect('title is required', $model->errors)->hasKey('title');
         expect('link is required', $model->errors)->hasKey('link');
         expect('description is not required', $model->errors)->hasntKey('description');
-        expect('content is required', $model->errors)->hasKey('content');
+        expect('content is required', $model->errors)->hasKey('content');       
+        expect('active is required', $model->errors)->hasKey('active');
+        expect('static is not required', $model->errors)->hasntKey('static');
+    }
+    
+    public function testUniqueLink()
+    {
+        $model = new Page([
+            'menu_name' => 'title',
+            'title' => 'Title',
+            'link' => 'unique-page-name',
+            'description' => 'Description',
+            'content' => 'Content',
+            'active' => 1
+        ]);       
+        $model->save();
+        
+        $page = new Page([
+            'menu_name' => 'other title',
+            'title' => 'Other Title',
+            'link' => 'unique-page-name',
+            'description' => 'Other description',
+            'content' => 'Other content',
+            'active' => 0
+        ]);
+        
+        expect('page is not valid', $page->validate())->false();
+    }
+    
+    public function testValidLink()
+    {
+        $model = new Page([
+            'menu_name' => 'title',
+            'title' => 'Title',
+            'link' => 'Wrong link',
+            'description' => 'Description',
+            'content' => 'Content',
+            'active' => 1
+        ]);     
+        
+        expect('page is not valid', $model->validate())->false();
+        
+        $model->link = 'valid-link';
+       
+        expect('page is not valid', $model->validate())->true();
+    }
+
+
+    public function testSave()
+    {
+        $model = new Page([
+            'menu_name' => 'title',
+            'title' => 'Title',
+            'link' => 'page-name',
+            'description' => 'Description',
+            'content' => 'Content',
+            'active' => 1,
+            'static' => 1
+        ]);
+
+        expect('model is saved', $model->save())->true();
+        expect('created_at is correct', $model->created_at)->notEmpty();
+        expect('updated_at is correct', $model->updated_at)->notEmpty();
     }
 }
