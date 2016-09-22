307d17b926b523f2890f852796096547c362d8d2
Bulat Damdinov <dbulats88@gmail.com>
2016-09-22 08:44:45 +0800

    added BookCreatePage for test page and changed BookCest functional test file

---
+++ tests/codeception/_pages/BookCreatePage.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace tests\codeception\_pages;
+
+use yii\codeception\BasePage;
+
+class BookCreatePage extends BasePage
+{
+    public $route = 'book/create';
+}
--- tests/codeception/functional/BookCest.php
+++ tests/codeception/functional/BookCest.php
@@ -3,9 +3,11 @@
 namespace tests\codeception\functional;
 
 use FunctionalTester;
+use tests\codeception\_pages\BookCreatePage;
 use Yii;
+use yii\helpers\Url;
 
-class BookCest
+class BookCest extends FunctionalCest
 {
     public function indexPage(FunctionalTester $I)
     {
@@ -16,5 +18,41 @@ class BookCest
         $I->click('Books');
 
         $I->see('Books', 'h1');
+    }
+
+    /**
+     * @before loginAsAdmin
+     * @after logout
+     */
+    public function createPageAsAdmin(FunctionalTester $I)
+    {
+        $I->wantTo('ensure that book create page works for admin');
+
+        BookCreatePage::openBy($I);
+        $I->seeInTitle('Create book');
+    }
+
+    /**
+     * @before loginAsModerator
+     * @after logout
+     */
+    public function createPageAsModerator(FunctionalTester $I)
+    {
+        $I->wantTo('ensure that book create page works for moderator');
+
+        BookCreatePage::openBy($I);
+        $I->seeInTitle('Create book');
+    }
+
+    /**
+     * @before loginAsUser
+     * @after logout
+     */
+    public function createPageAsUser(FunctionalTester $I)
+    {
+        $I->wantTo('ensure that book create page not works for simple user');
+
+        BookCreatePage::openBy($I);
+        $I->seeInTitle('Forbidden');
     }
 }
