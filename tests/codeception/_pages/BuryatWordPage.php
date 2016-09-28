4cdf8402ca343efa91ec6e985cd4e6892903d468
Bulat Damdinov <dbulats88@gmail.com>
2016-09-28 22:39:02 +0800

    Added test pages BuryatWordPage and RussianWordPage

---
+++ tests/codeception/_pages/BuryatWordPage.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace tests\codeception\_pages;
+
+use yii\codeception\BasePage;
+
+class BuryatWordPage extends BasePage
+{
+    public $route = 'buryat-word/index';
+}
---
+++ tests/codeception/_pages/RussianWordPage.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace tests\codeception\_pages;
+
+use yii\codeception\BasePage;
+
+class RussianWordPage extends BasePage
+{
+    public $route = 'russian-word/index';
+}
