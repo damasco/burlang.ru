517e5f54fcc9185bd05ab3438ea9fff8b590ab88
Bulat Damdinov <dbulats88@gmail.com>
2016-10-01 22:44:46 +0800

    changed acceptance/BookCest

--- tests/codeception/acceptance/BookCest.php
+++ tests/codeception/acceptance/BookCest.php
@@ -4,8 +4,9 @@ namespace tests\codeception\acceptance;
 
 use AcceptanceTester;
 use Yii;
+use tests\codeception\_pages\BookCreatePage;
 
-class BookCest
+class BookCest extends AcceptanceCest
 {
     public function indexPage(AcceptanceTester $I)
     {
@@ -19,5 +20,41 @@ class BookCest
             $I->wait(2); // only for selenuim
         }
         $I->see('Books', 'h1');
+    }
+
+    /**
+     * @before loginAsAdmin
+     * @after logout
+     */
+    public function createPageAsAdmin(AcceptanceTester $I)
+    {
+        $I->wantTo('ensure that book create page works for admin');
+
+        BookCreatePage::openBy($I);
+        $I->seeInTitle('Create book');
+    }
+
+    /**
+     * @before loginAsModerator
+     * @after logout
+     */
+    public function createPageAsModerator(AcceptanceTester $I)
+    {
+        $I->wantTo('ensure that book create page works for moderator');
+
+        BookCreatePage::openBy($I);
+        $I->seeInTitle('Create book');
+    }
+
+    /**
+     * @before loginAsUser
+     * @after logout
+     */
+    public function createPageAsUser(AcceptanceTester $I)
+    {
+        $I->wantTo('ensure that book create page not works for simple user');
+
+        BookCreatePage::openBy($I);
+        $I->seeInTitle('Forbidden');
     }
 }
