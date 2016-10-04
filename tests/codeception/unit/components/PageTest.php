005fca2fa42f676a49091b3334033c87c8824680
Bulat Damdinov <dbulats88@gmail.com>
2016-10-04 23:04:16 +0800

    changed  unit/components/PageTest

--- tests/codeception/unit/components/PageTest.php
+++ tests/codeception/unit/components/PageTest.php
@@ -5,6 +5,7 @@ namespace test\codeception\unit\components;
 use app\components\Page;
 use Codeception\Specify;
 use yii\codeception\DbTestCase;
+use app\models\Page as PageModel;
 
 class PageTest extends DbTestCase
 {
@@ -14,5 +15,26 @@ class PageTest extends DbTestCase
     {
         $page = new Page();
         expect('return null string', $page->menuItem('hello'))->equals('');
+    }
+
+    public function testMenuItem()
+    {
+        $pageName = 'about';
+
+        /** @var PageModel $model */
+        $model = PageModel::findOne(['link' => $pageName]);
+        $model->active = 1;
+        $model->save();
+
+        $page = new Page();
+
+        $request = $this->getMock('\yii\web\Request', ['getUrl']);
+        $request->expects($this->any())
+            ->method('getUrl')
+            ->will($this->returnValue('/page/' . $pageName));
+
+        \Yii::$app->set('request', $request);
+
+        expect('return about string', $page->menuItem($pageName))->hasKey('label');
     }
 }
