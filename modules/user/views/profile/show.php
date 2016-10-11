83a953b3fb4c1f447b2d20a66ba6651252c52fb1
Bulat Damdinov <dbulats88@gmail.com>
2016-10-11 23:49:09 +0800

    changed profile page

--- modules/user/views/profile/show.php
+++ modules/user/views/profile/show.php
@@ -20,46 +20,57 @@ $this->title = empty($profile->name) ? Html::encode($profile->user->username) : 
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="row">
-    <div class="col-xs-12 col-sm-6 col-md-6">
-        <div class="row">
-            <div class="col-sm-6 col-md-4">
+    <div class="row">
+        <div class="col-sm-3">
+            <p>
                 <?= Html::img($profile->getAvatarUrl(230), [
                     'class' => 'img-rounded img-responsive',
                     'alt' => $profile->user->username,
                 ]) ?>
-            </div>
-            <div class="col-sm-6 col-md-8">
-                <?php if (Yii::$app->user->identity->id == $profile->user->id): ?>
-                    <?= Html::a(
-                        Html::icon('cog') . ' ' . Yii::t('user', 'Profile settings'),
-                        ['/user/settings/profile'],
-                        ['class' => 'btn btn-default']
-                    ) ?>
-                <?php endif ?>
-                <h4><?= $this->title ?></h4>
-                <ul style="padding: 0; list-style: none outside none;">
-                    <?php if (!empty($profile->location)): ?>
-                        <li>
-                            <i class="glyphicon glyphicon-map-marker text-muted"></i> <?= Html::encode($profile->location) ?>
-                        </li>
-                    <?php endif; ?>
-                    <?php if (!empty($profile->website)): ?>
-                        <li>
-                            <i class="glyphicon glyphicon-globe text-muted"></i> <?= Html::a(Html::encode($profile->website), Html::encode($profile->website)) ?>
-                        </li>
-                    <?php endif; ?>
-                    <?php if (!empty($profile->public_email)): ?>
-                        <li>
-                            <i class="glyphicon glyphicon-envelope text-muted"></i> <?= Html::a(Html::encode($profile->public_email), 'mailto:' . Html::encode($profile->public_email)) ?>
-                        </li>
-                    <?php endif; ?>
+            </p>
+        </div>
+        <div class="col-sm-9">
+            <?php if (Yii::$app->user->identity->id == $profile->user->id): ?>
+                <?= Html::a(
+                    Html::icon('cog') . ' ' . Yii::t('user', 'Profile settings'),
+                    ['/user/settings/profile'],
+                    ['class' => 'btn btn-default']
+                ) ?>
+            <?php endif ?>
+            <h4><?= $this->title ?></h4>
+            <ul style="padding: 0; list-style: none outside none;">
+                <?php if (!empty($profile->location)): ?>
                     <li>
-                        <i class="glyphicon glyphicon-time text-muted"></i> <?= Yii::t('user', 'Joined on {0, date}', $profile->user->created_at) ?>
+                        <i class="glyphicon glyphicon-map-marker text-muted"></i> <?= Html::encode($profile->location) ?>
                     </li>
-                </ul>
-                <?php if (!empty($profile->bio)): ?>
-                    <p><?= Html::encode($profile->bio) ?></p>
                 <?php endif; ?>
+                <?php if (!empty($profile->website)): ?>
+                    <li>
+                        <i class="glyphicon glyphicon-globe text-muted"></i> <?= Html::a(Html::encode($profile->website), Html::encode($profile->website)) ?>
+                    </li>
+                <?php endif; ?>
+                <?php if (!empty($profile->public_email)): ?>
+                    <li>
+                        <i class="glyphicon glyphicon-envelope text-muted"></i> <?= Html::a(Html::encode($profile->public_email), 'mailto:' . Html::encode($profile->public_email)) ?>
+                    </li>
+                <?php endif; ?>
+                <li>
+                    <i class="glyphicon glyphicon-time text-muted"></i> <?= Yii::t('user', 'Joined on {0, date}', $profile->user->created_at) ?>
+                </li>
+            </ul>
+            <?php if (!empty($profile->bio)): ?>
+                <p><?= Html::encode($profile->bio) ?></p>
+            <?php endif; ?>
+
+            <div class="panel panel-default">
+                <div class="panel-heading"><?= Yii::t('app', 'Statistics') ?></div>
+                <div class="panel-body">
+                    <ul>
+                        <li><?= Yii::t('app', 'Names') ?>: <?= $profile->user->getBuryatNames()->count() ?></li>
+                        <li><?= Yii::t('app', 'Buryat words') ?>: <?= $profile->user->getBuryatWords()->count() ?></li>
+                        <li><?= Yii::t('app', 'Russian words') ?>: <?= $profile->user->getRussianWords()->count() ?></li>
+                    </ul>
+                </div>
             </div>
         </div>
     </div>
