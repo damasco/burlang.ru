d34194715034ddb7d5cb4dad86088252b118465e
Bulat Damdinov <dbulats88@gmail.com>
2016-10-23 20:12:16 +0800

    changed views for statistics

--- views/statistics/_menu.php
+++ views/statistics/_menu.php
@@ -8,8 +8,16 @@
 
 <?= \yii\widgets\Menu::widget([
     'items' => [
-        ['label' => Yii::t('app', 'Data'), 'url' => ['index'], 'active' => Yii::$app->controller->action->id == 'index'],
-        ['label' => Yii::t('app', 'Search'), 'url' => ['search'], 'active' => Yii::$app->controller->action->id == 'search'],
+        [
+            'label' => Yii::t('app', 'Data'),
+            'url' => ['index'],
+            'active' => Yii::$app->controller->action->id == 'index'
+        ],
+        [
+            'label' => Yii::t('app', 'Search'),
+            'url' => ['search'],
+            'active' => Yii::$app->controller->action->id == 'search'
+        ],
     ],
     'options' => [
         'class' => 'nav nav-tabs'
