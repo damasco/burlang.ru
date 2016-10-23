b08f62d3c7aa2f7e1ce6ee8984a94880a69c8ba3
Bulat Damdinov <dbulats88@gmail.com>
2016-10-23 20:21:04 +0800

    changed BuryatWordManager

--- components/BuryatWordManager.php
+++ components/BuryatWordManager.php
@@ -9,14 +9,14 @@ use app\models\SearchData;
 class BuryatWordManager
 {
     /**
-     * @param string $str
+     * @param string $q
      * @return array
      */
-    public function getWordsWithFilter($str)
+    public function getWordsWithFilter($q)
     {
         return BuryatWord::find()
             ->select(['name as value'])
-            ->filterWhere(['like', 'name', $str . '%', false])
+            ->filterWhere(['like', 'name', $q . '%', false])
             ->orderBy('name')
             ->limit(10)
             ->asArray()
@@ -24,19 +24,19 @@ class BuryatWordManager
     }
 
     /**
-     * @param string $str
+     * @param string $q
      * @return boolean|null|array
      */
-    public function getTranslations($str)
+    public function getTranslations($q)
     {
-        if (StringHelper::isWord($str)) {
+        if (StringHelper::isWord($q)) {
             /** @var BuryatWord $word */
-            $word = BuryatWord::findOne(['name' => $str]);
+            $word = BuryatWord::findOne(['name' => $q]);
 
             if ($word && $word->getTranslations()->exists()) {
                 return $word->getTranslations()->asArray()->all();
             } else {
-                (new SearchDataCreator($str, SearchData::BURYAT_WORD_TYPE))->execute();
+                (new SearchDataCreator($q, SearchData::BURYAT_WORD_TYPE))->execute();
                 return null;
             }
         } else {
