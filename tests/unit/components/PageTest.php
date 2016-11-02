9923384a1b4ff122a3eb25176a93aa2bd76bef68
Bulat Damdinov <dbulats88@gmail.com>
2016-11-02 23:29:40 +0800

    fixed unit components PageTest

--- tests/unit/components/PageTest.php
+++ tests/unit/components/PageTest.php
@@ -3,6 +3,7 @@
 namespace test\unit\components;
 
 use app\components\Page;
+use app\modules\user\models\User;
 use Codeception\Specify;
 use Codeception\Test\Unit;
 use app\models\Page as PageModel;
@@ -21,6 +22,7 @@ class PageTest extends Unit
     {
         $pageName = 'about';
 
+        \Yii::$app->user->login(new User(['id' => 1]));
         /** @var PageModel $model */
         $model = PageModel::findOne(['link' => $pageName]);
         $model->active = 1;
