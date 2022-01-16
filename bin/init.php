6d038c4b768f9ffb56db6294a4abc98b6a30b87f
Bulat Damdinov <dbulats88@gmail.com>
2022-01-16 21:02:24 +0200

    Rename bin/init to bin/init.php

--- bin/init.php
+++ bin/init.php
@@ -1,4 +1,3 @@
-#!/usr/bin/env php
 <?php
 /**
  * Yii Application Initialization Tool
--- environments/prod/public/index.php
+++ environments/prod/public/index.php
@@ -1,6 +1,4 @@
 <?php
-
-// comment out the following two lines when deployed to production
 defined('YII_DEBUG') or define('YII_DEBUG', false);
 defined('YII_ENV') or define('YII_ENV', 'prod');
 
