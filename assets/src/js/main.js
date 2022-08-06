f9312eea6433f438f39840d87587103e7cb9f3c8
Bulat Damdinov <dbulats88@gmail.com>
2022-08-06 23:05:19 +0800

    Remove extra code from main.js

--- assets/src/js/main.js
+++ assets/src/js/main.js
@@ -4,20 +4,5 @@ $(document).ready(function () {
     $this.parent('span').siblings('input').sendkeys($this.text());
   });
 
-  $('a.js-link-name').on('click', function (e) {
-    e.preventDefault();
-    let url = $('.buryat-name-list').data('url');
-    let name = $(this).text();
-    let $modal = $('#view-name-modal');
-    $.ajax({
-      url: url,
-      data: {name: name}
-    }).done(function (response) {
-      $modal.find('.modal-title').html(name);
-      $modal.find('.modal-body').html(response);
-      $modal.modal('show');
-    });
-  });
-
   $('.image-responsive-container img').addClass('img-responsive');
 });
